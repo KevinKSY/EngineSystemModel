2f284bca1428bfe1bdf73ffe6324f934d25e2010
KevinKSY <kevin.koosup.yum@ntnu.no>
2015-01-26 13:51:23 +0100

    Update parameters--

--- make.m
+++ make.m
@@ -32,6 +32,6 @@ mex ThdynCVConv.c ThdynCVConv_wrapper.c GetThdynCombGasZachV1.c -outdir ..
 mex ThdynCVConv1.c ThdynCVConv_wrapper1.c GetThdynCombGasZachV1.c -outdir ..
 mex turbine_calc_PTF.c turbine_calc_PTF_wrapper.c GetThdynCombGasZachV1.c -outdir ..
 mex turbine_calc_MV_PTF.c turbine_calc_MV_PTF_wrapper.c GetThdynCombGasZachV1.c -outdir ..
-mex InCylinderMV.c InCylinderMV_wrapper.c GetThdynCombGasZachV1.c -outdir ..
+mex -g InCylinderMV.c InCylinderMV_wrapper.c GetThdynCombGasZachV1.c -outdir ..
 mex GetThdynCombGasZachSFunc.c GetThdynCombGasZachV1.c -outdir ..
 cd(currFolder);
